18f8c1daaec4bb2667ae426103988408d123e9e4
Ashtonval <tennisjedi20xx@gmail.com>
2024-03-24 12:25:25 -0700

    up to fleeing
    up to fleeing

--- CrowdSimulation/Assets/AIControl.cs
+++ CrowdSimulation/Assets/AIControl.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,17 +6,30 @@ using UnityEngine.AI;
 
 public class AIControl : MonoBehaviour {
 
-    public GameObject goal;
+    GameObject[] goalLocations;
     NavMeshAgent agent;
+    Animator anim;
 
     void Start() {
 
         agent = GetComponent<NavMeshAgent>();
-        agent.SetDestination(goal.transform.position);
+        goalLocations = GameObject.FindGameObjectsWithTag("goal");
+        int i = UnityEngine.Random.Range(0, goalLocations.Length);
+        agent.SetDestination(goalLocations[i].transform.position);
+        anim = this.GetComponent<Animator>();
+        anim.SetTrigger("isWalking");
+        anim.SetFloat("Offset", UnityEngine.Random.Range(0.0f, 1.0f));
+        float sm = UnityEngine.Random.Range(0.5f, 2);
+        anim.SetFloat("speedMult", sm);
+        agent.speed *= sm;
     }
 
 
     void Update() {
-
+        if(agent.remainingDistance < 1)
+        {
+            int i = UnityEngine.Random.Range(0, goalLocations.Length);
+            agent.SetDestination(goalLocations[i].transform.position);
+        }
     }
 }
